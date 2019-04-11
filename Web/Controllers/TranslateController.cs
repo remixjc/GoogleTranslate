f0ca4e4db98e9622375044ad06d2d4d5b9e980eb
Jacob Wong <jacobangle7@gmail.com>
2019-04-11 14:17:34 +0800

    实现index.html翻译页面调用接口进行翻译的操作

---
+++ Web/Controllers/TranslateController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+
+namespace Web.Controllers
+{            
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TranslateController : ControllerBase
+    {
+        private readonly INodeServices _services;
+        public TranslateController(INodeServices services)
+        {
+            _services = services;
+        }
+            // GET: api/Translate
+            [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Translate/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Translate
+        [HttpPost]
+        public async Task<string> Post([FromForm] string fr)
+        {
+            string result = await Common.Common.Translate(fr, "auto", Common.Common.Language.en_US.ToString(), _services);
+            return result;
+        }
+
+        // PUT: api/Translate/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
